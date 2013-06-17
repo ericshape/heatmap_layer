483ac867f3214da1b3398bc2e91cab2993248f51
ericshape <wangjiprc@gmail.com>
2013-06-17 16:25:54 +0200

    sync with vs 2012

--- demo/maps_heatmap_layer/multi_layer.js
+++ demo/maps_heatmap_layer/multi_layer.js
@@ -1,6 +1,16 @@
 /*
-    multi layer libs in Backbone js
-*/
+ multi layer libs in Backbone js
+ */
+
+/////////////////////////////////
+////  Add reference. 
+/////////////////////////////////
+///<reference path="GetVehicleLoad_2012-03-01_tram_120mn.js"/>
+///<reference path="Cities.xml"/>
+
+/*
+ multi layer libs in Backbone js
+ */
 ////////////////////////////////////
 ///  Configuration File Loading
 ///////////////////////////////////
@@ -29,7 +39,7 @@ checkXMLDocObj = function (xmlFile) {
 ///////////////////////////////////
 var timestamp_hashtable = new Array();
 
-var loadData = function(map, animiationLayer, id) {
+var loadData = function (map, animiationLayer, id) {
 
     var routeLines = [];
 
@@ -42,9 +52,9 @@ var loadData = function(map, animiationLayer, id) {
 
     //		var json_data_heatmap = [];
 
-//        $.getJSON( function(BusMoveData) {
+    //        $.getJSON( function(BusMoveData) {
     //change the xrce data format to heatmap data format
-//                console.log(BusMoveData);
+    //                console.log(BusMoveData);
 
     var i = 0;
 
@@ -56,23 +66,19 @@ var loadData = function(map, animiationLayer, id) {
     var cur_timestamp = 0;
     var LanLat_Array;
 
-    $.each(BusMoveData, function(arrayID, value)
-    {
+    $.each(BusMoveData, function (arrayID, value) {
         //number of entity in the JSON file.
         i++;
 
         var time_string = value.Date.replace(/\(|\)/g, "");
 
-        cur_timestamp = time_string.substr(5,13);
+        cur_timestamp = time_string.substr(5, 13);
 
-        if (cur_timestamp == pre_timestamp)
-        {
-            LanLat_Array.push({"lat": value.Y, "lon":value.X});
+        if (cur_timestamp == pre_timestamp) {
+            LanLat_Array.push({ "lat": value.Y, "lon": value.X });
         }
-        else
-        {
-            if (LanLat_Array != null)
-            {
+        else {
+            if (LanLat_Array != null) {
                 timestamp_hashtable[pre_timestamp] = LanLat_Array;
             }
             pre_timestamp = cur_timestamp;
@@ -81,7 +87,7 @@ var loadData = function(map, animiationLayer, id) {
 
     });
 
-//                console.log(timestamp_hashtable);
+    //                console.log(timestamp_hashtable);
 
 
     /*create array:*/
@@ -89,14 +95,13 @@ var loadData = function(map, animiationLayer, id) {
     var timestamp_number = [];
     var timestamp_number_id = 0;
 
-    for (k in timestamp_hashtable)
-    {
+    for (k in timestamp_hashtable) {
 
         timestamp_number.push(k);
     }
 
 
-    var drawBus = function(timestamp_id){
+    var drawBus = function (timestamp_id) {
 
         items = timestamp_hashtable[timestamp_number[timestamp_id]];
 
@@ -106,8 +111,7 @@ var loadData = function(map, animiationLayer, id) {
 
 
         //pushing items into array each by each and then add markers
-        for(var j=0;j<items.length;j++)
-        {
+        for (var j = 0; j < items.length; j++) {
             var LamMarker = new L.marker([items[j].lat, items[j].lon]);
             marker.push(LamMarker);
 
@@ -117,20 +121,17 @@ var loadData = function(map, animiationLayer, id) {
             .addTo(animiationLayer);
 
 
-        setTimeout(function(){
+        setTimeout(function () {
 
-            for(var j=0;j<marker.length;j++)
-            {
-                if (timestamp_number_id < timestamp_number.length)
-                {
+            for (var j = 0; j < marker.length; j++) {
+                if (timestamp_number_id < timestamp_number.length) {
                     map.removeLayer(marker[j]);
                 }
             }
             //clear marker
             marker = [];
 
-            if (timestamp_number_id < timestamp_number.length)
-            {
+            if (timestamp_number_id < timestamp_number.length) {
                 timestamp_number_id++;
                 drawBus(timestamp_number_id);
             }
@@ -143,143 +144,155 @@ var loadData = function(map, animiationLayer, id) {
 };
 
 
-    // Define the heatmpaData
-    var heatmapData = heatmapData || {};
+// Define the heatmpaData
+var heatmapData = heatmapData || {};
 
-    /////////////////////////
-    // heatmapData Model
-    ////////////////////////
-    heatmapData.data = Backbone.Model.extend({
-        defauts: {
-            lat: -37.8833,
-            lon: 145.167,
-            value: 1
+/////////////////////////
+// heatmapData Model
+////////////////////////
+heatmapData.data = Backbone.Model.extend({
+    defauts: {
+        lat: -37.8833,
+        lon: 145.167,
+        value: 1
+    }
+});
+
+/////////////////////////////
+// heatmapData Collection
+////////////////////////////
+var heatmapDataList = Backbone.Collection.extend({
+    // reference to the data model
+    model: heatmapData.data,
+
+    // store it in the local namespace storage
+    //					localStorage: new Store('heatmapData-backbone'),
+
+    // filter down the list when it complete
+    completed: function () {
+        return this.filter(function (heatmapdata) {
+            return heatmapdata.get('completed');
+        });
+    },
+
+    // Filter down the list to only heatmap that are still not finished.
+    remaining: function () {
+        return this.without.apply(this, this.completed());
+    }
+
+});
+
+
+var heatmapdata_list = new heatmapDataList();
+
+//////////////////////////////
+// heatmapData View
+/////////////////////////////
+heatmapData.mapView = Backbone.View.extend({
+    // binding existed html element
+    el: '#map',
+
+    initialize: function () {
+
+        // console output the file
+        var xmlDoc = checkXMLDocObj("Cities.xml");
+        var messages = xmlDoc.getElementsByTagName("TileUrls");
+
+        var tileIndex = xmlDoc.getElementsByTagName("TileUrls")[0].childNodes;
+        var tileContent = xmlDoc.getElementsByTagName("TileUrls")[0].firstChild;
+
+        console.log(tileIndex.length);
+
+        for (var i = 0; i < tileIndex.length; i++) {
+            console.log(tileContent.textContent);
+            tileContent = tileContent.nextSibling;
         }
-    });
 
-    /////////////////////////////
-    // heatmapData Collection
-    ////////////////////////////
-    var heatmapDataList = Backbone.Collection.extend({
-        // reference to the data model
-        model: heatmapData.data,
+        tileContent = xmlDoc.getElementsByTagName("TileUrls")[0].firstChild.nextSibling;
 
-        // store it in the local namespace storage
-//					localStorage: new Store('heatmapData-backbone'),
-
-        // filter down the list when it complete
-        completed: function () {
-            return this.filter(function (heatmapdata) {
-                return heatmapdata.get('completed');
-            });
-        },
-
-        // Filter down the list to only heatmap that are still not finished.
-        remaining: function () {
-            return this.without.apply(this, this.completed());
-        }
-
-    });
-
-
-    var heatmapdata_list = new heatmapDataList();
-
-    //////////////////////////////
-    // heatmapData View
-    /////////////////////////////
-    heatmapData.mapView = Backbone.View.extend({
-        // binding existed html element
-        el: '#map',
-
-        initialize: function(){
-
-            var baseLayer = L.tileLayer(
-                'http://{s}.tile.cloudmade.com/ad132e106cd246ec961bbdfbe0228fe8/997/256/{z}/{x}/{y}.png',{
-                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-                    maxZoom: 18
-                }
-            );
-
-
-            var animationLayer = L.layerGroup();
-
-
-            var heatmapLayer = L.TileLayer.heatMap({
-                // radius could be absolute or relative
-                // absolute: radius in meters, relative: radius in pixels
-                radius: { value: 15000, absolute: true },
-                //radius: { value: 20, absolute: false },
-                opacity: 0.8,
-                gradient: {
-                    0.45: "rgb(0,0,255)",
-                    0.55: "rgb(0,255,255)",
-                    0.65: "rgb(0,255,0)",
-                    0.95: "yellow",
-                    1.0: "rgb(255,0,0)"
-                }
-            });
-
-            //Add heatmap data to grenoble area for debug.
-            for (var i = 1; i<100; i++){
-                testData.data.push({
-                    lat: i*0.01 + 48.68,
-                    lon: i*0.01 +6.17,
-                    value: i
-                });
+        var colorLayer = L.tileLayer(
+            'http://spider:56721/ColorFull/{z}/{x}/{y}.png ', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                maxZoom: 18
             }
-            heatmapLayer.setData(testData.data);
+        );
 
-            var map = new L.Map('map', {
-                center: new L.LatLng(48.68, 6.17),
-                zoom: 13,
-                layers: [baseLayer, heatmapLayer, animationLayer]
+
+        var baseLayer = L.tileLayer(
+            'http://{s}.tile.cloudmade.com/ad132e106cd246ec961bbdfbe0228fe8/997/256/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                maxZoom: 18
+            }
+        );
+
+
+        var animationLayer = L.layerGroup();
+
+
+        var heatmapLayer = L.TileLayer.heatMap({
+            // radius could be absolute or relative
+            // absolute: radius in meters, relative: radius in pixels
+            radius: { value: 15000, absolute: true },
+            //radius: { value: 20, absolute: false },
+            opacity: 0.8,
+            gradient: {
+                0.45: "rgb(0,0,255)",
+                0.55: "rgb(0,255,255)",
+                0.65: "rgb(0,255,0)",
+                0.95: "yellow",
+                1.0: "rgb(255,0,0)"
+            }
+        });
+
+        //Add heatmap data to grenoble area for debug.
+        for (var i = 1; i < 100; i++) {
+            testData.data.push({
+                lat: i * 0.01 + 48.68,
+                lon: i * 0.01 + 6.17,
+                value: i
             });
-
-            // make accessible for debugging
-            heatmapdata_listshow = heatmapdata_list;
-
-            heatmap_layer = heatmapLayer;
-            animation_layer = animationLayer;
-
-            loadData(map, animationLayer, 4);
-
-            var bikeIcon = L.icon({
-                iconUrl: 'marker-bike-green-shadowed.png',
-                iconSize: [25, 39],
-                iconAnchor: [12, 39],
-                shadowUrl: null
-            });
-
-
-            //add control part:
-            var overlayMaps = {
-                'Basemap': baseLayer,
-                'Heatmap': heatmapLayer,
-                'Animation': animationLayer
-            };
-            var controls = L.control.layers(null, overlayMaps, {collapsed: false});
-            controls.addTo(map);
         }
+        heatmapLayer.setData(testData.data);
+
+        var map = new L.Map('map', {
+            center: new L.LatLng(48.68, 6.17),
+            zoom: 13,
+            layers: [colorLayer, heatmapLayer, animationLayer]
+        });
+
+        // make accessible for debugging
+        heatmapdata_listshow = heatmapdata_list;
+
+        heatmap_layer = heatmapLayer;
+        animation_layer = animationLayer;
+
+        loadData(map, animationLayer, 4);
+
+        var bikeIcon = L.icon({
+            iconUrl: 'marker-bike-green-shadowed.png',
+            iconSize: [25, 39],
+            iconAnchor: [12, 39],
+            shadowUrl: null
+        });
 
 
-    });
-
-    // console output the file
-    var xmlDoc=checkXMLDocObj("Cities.xml");
-
-    var tileIndex = xmlDoc.getElementsByTagName("TileUrls")[0].childNodes;
-    var tileContent = xmlDoc.getElementsByTagName("TileUrls")[0].firstChild;
-
-    console.log(tileIndex.length);
-
-    for (var i=0; i<tileIndex.length; i++){
-        console.log(tileContent.textContent);
-        tileContent = tileContent.nextSibling;
+        //add control part:
+        var overlayMaps = {
+            'Xtilemap': colorLayer,
+            'Heatmap': heatmapLayer,
+            'Animation': animationLayer
+        };
+        var controls = L.control.layers(null, overlayMaps, { collapsed: false });
+        controls.addTo(map);
     }
 
 
-    //make a instance for the heatmap_mapview launch
-    var heatmap_mapView = new heatmapData.mapView();
+});
+
+
+
+//make a instance for the heatmap_mapview launch
+var heatmap_mapView = new heatmapData.mapView();
 
 
 
@@ -308,4 +321,3 @@ var loadData = function(map, animiationLayer, id) {
 //    map.addLayer(new L.TileLayer(config.tileUrl, {attribution: config.tileAttrib}));
 //    map.addLayer(new L.TileLayer(config.overlayTileUrl));
 //    map.setView(config.initLatLng, config.initZoom);
-
